c2e73de3179340536f4f5a35ff457cda09320133
kressha <61380550+kressha@users.noreply.github.com>
2020-10-01 18:47:48 +0800

    working front end sa append ingredients

--- public/js/appendIngredient.js
+++ public/js/appendIngredient.js
@@ -1,66 +1,77 @@
-var ingArray = [];
+$(document).ready(function(){ 
+    var ingArray = [];
 
-function addIng(id, qty, unit) {
-    ingArray.push({id: id, qty: Number.parseInt(qty), unit: unit});
-}
-
-$(document).ready(function(){
+    function addIng(id, qty, unit) {
+        ingArray.push({id: id, qty: Number.parseInt(qty), unit: unit});
+    }
     
-    var id; 
-
+    var i=1;  
     var idIngList = [], ingName = [], qtyList = [], unitList = [];
 
-    $("#addIngredient").click(function(){
+  $('#addIngredient').click(function(){  
 
-        id = $("#selectIng").attr('data-id');
-        var selectedValue = selectIng.options[selectIng.selectedIndex].value;
-        var selectedVal = selectIng.options[selectIng.selectedIndex].getAttribute("data-id");
+    console.log("adding...");
+    var selectIng = document.getElementById("selectIng");
+    var selectedValue = selectIng.options[selectIng.selectedIndex].value;
+    var selectedVal = selectIng.options[selectIng.selectedIndex].getAttribute("data-id");
+    var qty = document.getElementById("quantity").value;
+    var unit = document.getElementById("unit").value;
 
-        var qty = document.getElementById("quantity").value;
-        console.log(qty); 
-        var unit = document.getElementById("unit").value;
-       // var unit = unit.options[unit.selectedIndex].value;
-       console.log(selectedValue);
-       console.log(selectedVal);
-       console.log("selectedValue");
+    idIngList.push(selectedVal);
+    ingName.push(selectedValue);
+    qtyList.push(qty);
+    unitList.push(unit);
 
-        idIngList.push(selectedValue);
-        ingName.push(selectedVal);
-        qtyList.push(qty);
-        unitList.push(unit); 
-        addIng(selectedValue, qty, unit);
+    addIng(selectedValue, qty, unit);
 
-        var html = '<div class="row"><span>'+ $("#selectIng").val() + '</span> <span>' + $("#quantity").val() + '</span><span>'+ $("#unit").val() +'</span><button type="button" style="margin-left: 25px; width: 80px" id="remove" class="btn btn-primary btn-sm can-btn">Remove</button></div>';
-        $(".col-12").append(html);
+    console.log(idIngList); 
+    console.log(ingName); 
+    console.log(qtyList);
+    console.log(unitList); 
 
-        console.log(idIngList); 
-        console.log(ingName); 
-        console.log(qtyList);
-        console.log(unitList); 
-    });
+    i++;  
+    $('#dynamic_field').append('<tr id="row'+i+'"><td><input disabled="disabled" class="form-control" id="selectIng" name="ingredientName" placeholder="' + selectedValue +'" style="width:215px;"></input></td><td><input type="number" class="form-control" disabled="disabled" style="width:120px; height: 40px; margin-right: 20px; margin-left: 20px;" id="quantity" placeholder="' + qty +'"min="1"></td><td><input id="unit" class="form-control" disabled="disabled" placeholder="' + unit +'" style="width:150px; height: 40px;"></input></td><td><button style="margin-left: 25px; height: 40px;" type="button" name="remove" id="'+i+'" class="btn btn-danger btn_remove">x Remove</button></td></tr>');  
+    });  
 
-    $("#remove").on("click", "#remove", function(){
-        $(this).parent().remove();
-    });
-   
+  $(document).on('click', '.btn_remove', function(){  
+         var button_id = $(this).attr("id");
+         var index = button_id - 2; 
+         console.log("removing..." + button_id);  
+         $('#row'+button_id+'').remove();  
+         ingName.splice(index, 1);
+         console.log(ingName);
+  });  
+
     $("button.save-btn").click(function(){
-        $.ajax({
+      $.ajax({
 
-            url: '/products/add',
-            method: 'POST',
-            data: {
-                idIngList: idIngList,
-                ingName: ingName,
-                qtyList: qtyList,
-                unitList: unitList
-                
-            },
-            error: () => callback(),
-            success: a => callback(a)
-        });
-        window.location.replace("/products/add");
-        alert("Product successfully saved!");
-    });
+          url: '/products/add',
+          method: 'POST',
+          data: {
+              idIngList: idIngList,
+              ingName: ingName,
+              qtyList: qtyList,
+              unitList: unitList
+              
+          },
+          error: () => callback(),
+          success: a => callback(a)
+      });
+      window.location.replace("/products/add");
+      alert("Product successfully saved!");
+  });
     
-});
+}); 
 
+// $(document).ready(function(){
+
+//     $(this).on("click", "#addIngredient", function(){
+//         var html = '<div class="row"><span>'+ $("#selectIng").val() + '</span> <span>' + $("#quantity").val() + '</span><span>'+ $("#unit").val() +'</span><button type="button" style="margin-left: 25px; width: 80px" id="remove" class="btn btn-primary btn-sm can-btn">Remove</button></div>';
+//         $(".col-12").append(html);
+//     });
+    
+//     $(this).on("click", "#remove", function(){
+//         $(this).parent().remove();
+//     });
+    
+// });
