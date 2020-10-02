d40721c1e4ceb6965798c662d89ffba7f6ad077b
deasisjhose <61381587+deasisjhose@users.noreply.github.com>
2020-10-03 05:10:12 +0800

    validator added

--- public/js/purchase.js
+++ public/js/purchase.js
@@ -1,27 +1,33 @@
-$(document).ready(function(){ 
-    $("button.save-btn").click(function(){
+$(document).ready(function() {
+    $("button.save-btn").click(function() {
         var purchDate = document.getElementById("purchDate").value;
         var expDate = document.getElementById("expDate").value;
         var numItems = document.getElementById("numItems").value;
         var purchPrice = document.getElementById("purchPrice").value;
         var supplyName = document.getElementById("supplyName");
         var supplyID = supplyName.options[supplyName.selectedIndex].getAttribute("data-id");
-        
-        $.ajax({
-            url: '/purchase/add',
-            method: 'POST',
-            data: {
-                purchDate: purchDate,
-                supplyName: supplyID,
-                numItems: numItems,
-                expDate: expDate, 
-                purchPrice: purchPrice
-            },
-            error: () => callback(),
-            success: function(){
-                alert("Purchase successfully added!");
-                window.location.href="/procurement"; 
-            }
-        });
+
+        if (purchDate == "" || expDate == "" || numItems == "" || purchPrice == "" || supplyName == "Select Supply") {
+            alert("Incomplete data");
+        } else if (numItems < 0 || purchPrice < 0) {
+            alert("Negative number of items and purchase price is invalid")
+        } else {
+            $.ajax({
+                url: '/purchase/add',
+                method: 'POST',
+                data: {
+                    purchDate: purchDate,
+                    supplyName: supplyID,
+                    numItems: numItems,
+                    expDate: expDate,
+                    purchPrice: purchPrice
+                },
+                error: () => callback(),
+                success: function() {
+                    alert("Purchase successfully added!");
+                    window.location.href = "/procurement";
+                }
+            });
+        }
     });
 });
